7ea4baa7637eac8186c5b71d7ac35a4d712b1484
Felix <neostyxde@gmail.com>
2020-11-19 21:59:39 +0100

    Download

--- lib/main.py
+++ lib/main.py
@@ -1,18 +1,28 @@
 import tkinter
 import webbrowser
+import youtube_dl
 from functools import partial
 from youtube_search import YoutubeSearch
 
 win = tkinter.Tk()
 
-def search():
-    results = YoutubeSearch("legends never die lol", max_results=10).to_dict()
-    for video in results:
-        print(video["id"])
-        x = tkinter.Button(win, text=video["title"], command=partial(webOpen, video["id"])).pack()
+searchInp = tkinter.Entry(win)
+searchInp.pack()
 
-searchInp = tkinter.Entry(win).pack()
-searchBtn = tkinter.Button(win, command=search).pack()
+def playVideo(videoId):
+    videoUrl = "https://youtu.be/" + videoId
+    ydl_opts = {
+        'outtmpl': dir_utils.get_perm_med_dir() + f'/sound_board/{name}.wav',
+        }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([videoUrl])
+def search():
+    searchQuery = searchInp.get()
+    results = YoutubeSearch(searchQuery, max_results=10).to_dict()
+    for video in results:
+        x = tkinter.Button(win, text=video["title"], command=partial(playVideo, video["id"])).pack()
+
+searchBtn = tkinter.Button(win, command=search, text="Go").pack()
 
 # print(results)
 
