63043249587ab47252ffcbee879a74e89772b025
Felix <neostyxde@gmail.com>
2020-11-20 07:25:24 +0100

    idk

--- lib/main.py
+++ lib/main.py
@@ -1,18 +1,22 @@
 import tkinter
 import webbrowser
 import youtube_dl
+from pathlib import Path
 from functools import partial
 from youtube_search import YoutubeSearch
 
 win = tkinter.Tk()
+win.title("MagicWaves")
+win.config(bg="#151515")
+win.geometry("400x400")
 
 searchInp = tkinter.Entry(win)
 searchInp.pack()
 
-def playVideo(videoId):
+def playVideo(videoId, videoName):
     videoUrl = "https://youtu.be/" + videoId
     ydl_opts = {
-        'outtmpl': dir_utils.get_perm_med_dir() + f'/sound_board/{name}.wav',
+        'outtmpl': str(Path.home()) + f'/sound_board/{videeoName}.wav',
         }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([videoUrl])
@@ -20,7 +24,9 @@ def search():
     searchQuery = searchInp.get()
     results = YoutubeSearch(searchQuery, max_results=10).to_dict()
     for video in results:
-        x = tkinter.Button(win, text=video["title"], command=partial(playVideo, video["id"])).pack()
+        print(video)
+        x = tkinter.Button(win, text=video["title"], command=partial(playVideo, videoId=video["id"]), videoName=video["title"])
+        x.pack()
 
 searchBtn = tkinter.Button(win, command=search, text="Go").pack()
 
